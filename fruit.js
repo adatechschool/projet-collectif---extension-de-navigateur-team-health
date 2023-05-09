197ce5d19325d2c24dbf1d655670edf7b754b20c
Djeneba Soumare <djenebasoum98@icloud.com>
2023-05-09 16:21:17 +0200

    emoji if high sugar

--- fruit.js
+++ fruit.js
@@ -1,5 +1,19 @@
 const API_KEY = "BlTQnbqdr3mjHb0gOvKxYDUlEPzrqUaOTeqOZInr"; // enlever
+const isFoodGood = (food) => {
+  // Check if the food item is low in added sugars and unhealthy fats
+  if (food['fat_saturated_g'] > 5 || food['sugar_g'] / food['serving_size_g'] > 0.2) {
+      return false;
+  }
 
+  // Check if the food item is safe and free from contaminants
+  if (food['sodium_mg'] > 2300 || food['cholesterol_mg'] > 300) {
+      return false;
+  }
+
+  // If all False food is healthy
+
+  return true;
+}
 const fruit = async () => {
   const fruitValue = window.getSelection().toString();
   // Fetch the data from the Api
@@ -18,39 +32,30 @@ const fruit = async () => {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
 
+
   const data = await response.json();
+   // Get the first food item from the data
+   const food = data[0];
+
+   // Check if the food is healthy or not
+   const isHealthy = isFoodGood(food);
+
+   // Set the emoji to display based on the healthiness of the food
+   const emoji = isHealthy ? '😊' : '🥶';
   // Manage the data and return a new div with the data
   if (data.length > 0) {
-    const fruitData = `<div class="fruit-data">
-                      <p>Name: ${data[0].name} </p>
-                      <p>Gramme: ${data[0].serving_size_g}</p>
-                       <p>Sugar: ${data[0].sugar_g}</p>
-                       <p>Calories: ${data[0].calories}</p>
-                       <p>Protein: ${data[0].protein_g}</p>
-                       <p>Fibre: ${data[0].fiber_g}</p>             
-                       </div>`;
-    // Check if a div element with class FruitData already exists
-    const existingFruitData = document.querySelector(".fruit-data");
-    // if (!existingFruitData) {
-    //   const selection = window.getSelection();
-    //   // checks if it is not an empty string using the !selection.isCollapsed statement.
-    //   // it creates a new div element using the document.createElement("div")
-    //   if (!selection.isCollapsed) {
-    //     // the code obtains the selected range using selection.getRangeAt(0)
-    //     const range = selection.getRangeAt(0);
-    //     // and inserts the new div element into the document using the
-    //     // range.insertNode(newNode)
-    //     const newNode = document.createElement("span");
-    //     newNode.innerHTML = FruitData;
-    //     range.insertNode(newNode);
-
-    //     // Add a mouseout event listener to the inserted element
-    //     newNode.addEventListener("mouseleave", () => {
-    //       newNode.remove();
-    //     });
-    //   }
-    // }
-    if (!existingFruitData) {
+    const fruitData =`<div class="fruit-data">
+    <p>Name: ${data[0].name} </p>
+    <p>Gramme: ${data[0].serving_size_g}</p>
+     <p>Sugar: ${data[0].sugar_g}</p>
+     <p>Calories: ${data[0].calories}</p>
+     <p>Protein: ${data[0].protein_g}</p>
+     <p>Fibre: ${data[0].fiber_g}</p>  
+     <p>isThisGood: ${emoji}</p>           
+     </div>`;
+    // Check if a div element with class fruit_data already exists
+    const existingfruit_data = document.querySelector(".fruit-data");
+    if (!existingfruit_data) {
       const selection = window.getSelection();
 
       if (!selection.isCollapsed) {
@@ -88,7 +93,6 @@ const fruit = async () => {
       }}
   }
 };
-
 
 // When i mouseup a word get the new div from the function above
 document.addEventListener("mouseup", async () => {
