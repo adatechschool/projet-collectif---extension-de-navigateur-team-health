f38b1284f1b5f6fe3d835e1d76ccb0437076490e
Bintou-Traore <bintou.traore24@outlook.fr>
2023-05-09 16:10:22 +0200

    css avec le pop up

--- fruit.js
+++ fruit.js
@@ -1,14 +1,14 @@
-const ApiKey = "BlTQnbqdr3mjHb0gOvKxYDUlEPzrqUaOTeqOZInr"; // enlever
+const API_KEY = "BlTQnbqdr3mjHb0gOvKxYDUlEPzrqUaOTeqOZInr"; // enlever
 
 const fruit = async () => {
-  const fruitvalue = window.getSelection().toString();
+  const fruitValue = window.getSelection().toString();
   // Fetch the data from the Api
   const response = await fetch(
-    `https://api.api-ninjas.com/v1/nutrition?query=${fruitvalue}`,
+    `https://api.api-ninjas.com/v1/nutrition?query=${fruitValue}`,
     {
       method: "GET",
       headers: {
-        "X-Api-Key": ApiKey,
+        "X-Api-Key": API_KEY,
         "Content-Type": "application/json",
       },
     }
@@ -21,7 +21,7 @@ const fruit = async () => {
   const data = await response.json();
   // Manage the data and return a new div with the data
   if (data.length > 0) {
-    const FruitData = `<div class="FruitData">
+    const fruitData = `<div class="fruit-data">
                       <p>Name: ${data[0].name} </p>
                       <p>Gramme: ${data[0].serving_size_g}</p>
                        <p>Sugar: ${data[0].sugar_g}</p>
@@ -30,7 +30,7 @@ const fruit = async () => {
                        <p>Fibre: ${data[0].fiber_g}</p>             
                        </div>`;
     // Check if a div element with class FruitData already exists
-    const existingFruitData = document.querySelector(".FruitData");
+    const existingFruitData = document.querySelector(".fruit-data");
     // if (!existingFruitData) {
     //   const selection = window.getSelection();
     //   // checks if it is not an empty string using the !selection.isCollapsed statement.
@@ -57,7 +57,7 @@ const fruit = async () => {
         const range = selection.getRangeAt(0);
 
         const newNode = document.createElement("span");
-        newNode.innerHTML = FruitData;
+        newNode.innerHTML = fruitData;
 
         // calculate the position of the selection relative to the document
         // This line gets the position of a selected text range 
@@ -92,8 +92,8 @@ const fruit = async () => {
 
 // When i mouseup a word get the new div from the function above
 document.addEventListener("mouseup", async () => {
-  const fruitvalue = window.getSelection().toString();
-  if (fruitvalue) {
+  const fruitValue = window.getSelection().toString();
+  if (fruitValue) {
     await fruit();
   }
 });
